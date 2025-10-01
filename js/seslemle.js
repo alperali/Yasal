cc0df5a137654412ed7d6e474dd5c7c132069e94
Alper <alper.goplay@gmail.com>
2025-10-01 23:35:50 +0300

    heceleme

---
+++ js/seslemle.js
@@ -0,0 +1,45 @@
+/*---------------------------------------------------------------------------
+ * Yasal, https://github.com/alperali/Yasal
+ * Telif Hakkı/Copyright A. Alper Atıcı. Her Hakkı Saklıdır.
+ * All Rights Reserved. This is not free software.
+ *---------------------------------------------------------------------------*/
+
+import hecele from './hecele.js';
+
+const nokim = /([\s\u00AD\u2010,;:.'"’“”!?\/()&#-]+)/;
+const k13 = /^([aeiouöüıİ])[\u00AD\u2010]|[\u00AD\u2010]([aeiouöüıİ])$/gi;
+const ptrn_xlit = /[âÂîÎûÛ]/g;
+let r;
+
+const translit = (() => {
+  const m = {
+    'â': 'a',
+    'Â': 'A',
+    'î': 'i',
+    'Î': 'İ',
+    'û': 'u',
+    'Û': 'U'
+  };
+  return (e => m[e]);
+})();
+
+self.addEventListener('message', e => {
+  switch (e.data.msg) {
+    case 'hazır':
+      // yukarıda sayfa yükleme tamamlanmış, biz de hazır olduğumuzu iletelim, yukarısı hecelenecek metinleri göndermeye başlasın varsa
+      postMessage({msg:'hazır'});
+      break;
+    case 'hecele':
+      r = e.data.hcl.replace(ptrn_xlit, translit)
+                .split(nokim)
+                .map((e) => {
+                    if (nokim.test(e)) return e;
+                    return hecele(e, hecele.SHY).replace(k13, '$1$2');
+                  })
+                .join('');
+      postMessage({msg:'yanıt', r});
+      break;
+    default:
+      console.error('seslemle worker: bilinmeyen mesaj geldi.');
+  }
+});
