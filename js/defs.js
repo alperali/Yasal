c545663b98d8d75ac762a77e18eb9f469677d115
A. Alper Atıcı <alper.goplay@gmail.com>
2025-10-07 10:30:37 +0300

    6735 eklendi.

--- js/defs.js
+++ js/defs.js
@@ -86,7 +86,8 @@ const belgeler = new Map([
   [ 6306, { başlık: 'AFET RİSKİ ALTINDAKİ ALANLARIN DÖNÜŞTÜRÜLMESİ HAKKINDA KANUN'} ],
   [ 6585, { başlık: 'PERAKENDE TİCARETİN DÜZENLENMESİ HAKKINDA KANUN'} ],
   [ 6326, { başlık: 'TURİST REHBERLİĞİ MESLEK KANUNU'} ],
-  [ 442,  { başlık: 'KÖY KANUNU'} ]
+  [ 442,  { başlık: 'KÖY KANUNU'} ],
+  [ 6735, { başlık: 'ULUSLARARASI İŞGÜCÜ KANUNU'} ]
 ]);
 
 export
