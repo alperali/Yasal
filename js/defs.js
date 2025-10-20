a959fc2f32b1aae34c59233ec7a3285f043f3a86
A. Alper Atıcı <alperali@live.com>
2025-10-20 10:34:12 +0300

    4483 eklendi.

--- js/defs.js
+++ js/defs.js
@@ -99,7 +99,8 @@ const belgeler = new Map([
   [ 1117, { başlık: 'KÜÇÜKLERİ MUZIR NEŞRİYATTAN KORUMA KANUNU'} ],
   [ 5324, { başlık: 'KOZMETİK KANUNU'} ],
   [ 3713, { başlık: 'TERÖRLE MÜCADELE KANUNU'} ],
-  [ 7179, { başlık: 'ASKERALMA KANUNU'} ]
+  [ 7179, { başlık: 'ASKERALMA KANUNU'} ],
+  [ 4483, { başlık: 'MEMURLAR VE DİĞER KAMU GÖREVLİLERİNİN YARGILANMASI HAKKINDA KANUN'} ]
 ]);
 
 export
