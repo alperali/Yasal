58f40bc3e7be0bab3214a9a03172ea30427f4917
A. Alper Atıcı <alperali@live.com>
2025-10-31 21:36:32 +0300

    5543 eklendi.

--- js/defs.js
+++ js/defs.js
@@ -101,6 +101,7 @@ const belgeler = new Map([
   [ 3713, { başlık: 'TERÖRLE MÜCADELE KANUNU'} ],
   [ 7179, { başlık: 'ASKERALMA KANUNU'} ],
   [ 2644, { başlık: 'TAPU KANUNU'} ],
+  [ 5543, { başlık: 'İSKÂN KANUNU'} ],
   [ 6191, { başlık: 'SÖZLEŞMELİ ERBAŞ VE ER KANUNU'} ],
   [ 5846, { başlık: 'FİKİR VE SANAT ESERLERİ KANUNU'} ],
   [ 6356, { başlık: 'SENDİKALAR VE TOPLU İŞ SÖZLEŞMESİ KANUNU'} ],
